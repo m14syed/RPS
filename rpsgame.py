94393ef33243fb85133b969cfe3ace39c4594903
m14syed <sohaibmsyed57@gmail.com>
2023-11-01 11:13:38 -0400

    RPSgame done

---
+++ rpsgame.py
@@ -0,0 +1,23 @@
+import sys
+import random
+
+userchoice = input("Please input Rock, Paper or, Scissors: \n")
+
+if userchoice != 'Rock' and userchoice != 'Paper' and userchoice !='Scissors':
+    sys.exit("Error: Please enter one of the available choices\n")
+print("\nYou have selected " + userchoice + ".")
+
+choices = ['Rock', 'Paper','Scissors']
+cpuchoice = random.choice(choices)
+print("Computer has chosen " + cpuchoice + ".")
+
+if cpuchoice == 'Paper' and userchoice == 'Rock':
+    print('\nYou have lost... 🥲')
+elif cpuchoice == 'Rock' and userchoice == 'Scissors':
+    print('\nYou have lost... 🥲')
+elif cpuchoice == 'Scissors' and userchoice == 'Paper':
+    print('\nYou have lost... 🥲')
+elif cpuchoice == userchoice:
+    print('\nYou have tied 😒')
+else:
+    print("\nYou have won! 😊")
