f14be9513e1257c18eaaebbd9de6e26fb9ac09e3
m14syed <sohaibmsyed57@gmail.com>
2023-11-01 23:07:02 -0400

    added method to stop the game

--- rpsgame.py
+++ rpsgame.py
@@ -1,26 +1,32 @@
 import sys
 import random
 
-print(title("Welcome to the RockPaperScissors Game!"))
+print(str.title("\nWelcome to the RockPaperScissors Game!\n\n"))
 
+playagain = True
 
-userchoice = input("Please input Rock, Paper or, Scissors: \n")
+while playagain:
+    userchoice = input("Please input Rock, Paper or, Scissors: \n")
 
-if userchoice != 'Rock' and userchoice != 'Paper' and userchoice !='Scissors':
-    sys.exit("Error: Please enter one of the available choices\n")
-print("\nYou have selected " + userchoice + ".")
+    if userchoice != 'Rock' and userchoice != 'Paper' and userchoice !='Scissors':
+        sys.exit("Error: Please enter one of the available choices\n")
+    print("\nYou have selected " + userchoice + ".")
 
-choices = ['Rock', 'Paper','Scissors']
-cpuchoice = random.choice(choices)
-print("Computer has chosen " + cpuchoice + ".")
+    choices = ['Rock', 'Paper','Scissors']
+    cpuchoice = random.choice(choices)
+    print("Computer has chosen " + cpuchoice + ".")
 
-if cpuchoice == 'Paper' and userchoice == 'Rock':
-    print('\nYou have lost... 🥲')
-elif cpuchoice == 'Rock' and userchoice == 'Scissors':
-    print('\nYou have lost... 🥲')
-elif cpuchoice == 'Scissors' and userchoice == 'Paper':
-    print('\nYou have lost... 🥲')
-elif cpuchoice == userchoice:
-    print('\nYou have tied 😒')
-else:
-    print("\nYou have won! 😊")
+    if cpuchoice == 'Paper' and userchoice == 'Rock':
+        print('\nYou have lost... 🥲\n')
+    elif cpuchoice == 'Rock' and userchoice == 'Scissors':
+        print('\nYou have lost... 🥲\n')
+    elif cpuchoice == 'Scissors' and userchoice == 'Paper':
+        print('\nYou have lost... 🥲\n')
+    elif cpuchoice == userchoice:
+        print('\nYou have tied 😒\n')
+    else:
+        print("\nYou have won! 😊\n")
+    
+    playagain = input("Input stop to exit the game or press any key to keep playing\n")
+    if playagain == 'stop':
+        playagain = False
