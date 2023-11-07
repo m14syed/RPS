a64790d4f1bfa58c7215ea89013fb4f3bf2a18c3
m14syed <sohaibmsyed57@gmail.com>
2023-11-07 11:48:57 -0500

    added command line input and welcome back msg

--- arcade.py
+++ arcade.py
@@ -1,25 +1,45 @@
 from rpsgame import RPS
 from choose_number import choosenumber
 import sys
+from termcolor import colored
 
-def arcade():
+def arcade(name = 'PlayerOne'):
     welcome_back = False
 
     while True:
         if welcome_back == True:
-            wbmsg = str.title("Welcome back to the Arcade")
-            print(wbmsg)
-
+            wbmsg = str.title(f"\nWelcome back to the Arcade {name}")
+            print(colored(wbmsg,'green', attrs=['bold']))
+      
         userchoice = input("\nPlease Select the Game You Wish to Play:\n1 = Rock, Paper Scissors\n2 = Guess the Number\n\nOr Select E to exit\n")
 
-        welcome_back == True
+        if userchoice not in ['1','2','E']:
+            errormsg = str.capitalize("Error: Please enter a correct input\n")
+            print(colored(errormsg, 'red', attrs = ['bold']))
+            return arcade(name)
+
+        welcome_back = True
 
         if userchoice == '1':
             RPS()
         elif userchoice == '2':
             choosenumber()
         else:
-            sys.exit ("See You Next Time! 👋")
+            sys.exit (f"See You Next Time {name}! 👋")
 
 if __name__ == "__main__":
-    arcade()
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description = 'Personalized Game Experience'
+    )
+
+    parser.add_argument(
+        '-n', '--name', metavar= 'name', required=True, help= 'Input your name'
+    )
+
+    args = parser.parse_args()
+
+    print(f"\n{args.name}, welcome to the Arcade! 🤖")
+
+    arcade(args.name)
